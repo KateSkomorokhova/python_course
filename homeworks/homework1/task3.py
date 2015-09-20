aeda9d7eab1f01ddfbc6aa647004dad650f86d13
KateSkomorokhova <ruhesommertag@gmail.com>
2015-09-20 22:37:30 +0300

    task

---
+++ homeworks/homework1/task3.py
@@ -0,0 +1,14 @@
+data = [int(i) for i in input().split()]
+uneven = data[0::2]
+uneven.sort()
+even = data [1::2]
+even.sort()
+even.reverse()
+
+i=0
+new_data = []
+count = len(even)
+while i< count:
+    new_data += [uneven[i], even[i]]
+    i+=1
+print (' '.join([str(i) for i in new_data]))
