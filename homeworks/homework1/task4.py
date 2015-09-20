d0e2f0e2ace5cc7ecbceb9a2315e9453de1225f2
KateSkomorokhova <ruhesommertag@gmail.com>
2015-09-20 22:38:39 +0300

    task

---
+++ homeworks/homework1/task4.py
@@ -0,0 +1,12 @@
+text = input()
+text_letter = set(text)
+count_letter = {}
+for letter in text_letter:
+    value = text.count(letter)
+    count_letter [letter] = value
+
+regularize = list(count_letter.keys())
+regularize.sort()
+
+for i in regularize:
+    print (i, count_letter[i])
