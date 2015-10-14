0e5f83932f49e388afecc760fb7dd23a38db2c4a
KateSkomorokhova <ruhesommertag@gmail.com>
2015-10-15 02:22:48 +0300

    task

---
+++ homeworks/homework2/task1.py
@@ -0,0 +1,10 @@
+def combinations(n, k):
+    if k > n:
+        return 0
+    elif k == 0:
+        return 1
+    else:
+        return (combinations(n-1, k-1) + combinations(n-1, k))
+
+n, k = [int(i) for i in input().split()]
+print(combinations(n, k))
