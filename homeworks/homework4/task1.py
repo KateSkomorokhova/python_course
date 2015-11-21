90cc3cb57f33a167ce9ac47f74a3d95ea151f8ec
KateSkomorokhova <ruhesommertag@gmail.com>
2015-11-21 21:27:07 +0300

    испр

---
+++ homeworks/homework4/task1.py
@@ -0,0 +1,27 @@
+a = open("yazkora.txt", "r")
+text = a.read().replace("\n", " ").split(".")
+
+def find_ka(x):
+    tmp = []
+    tmp += x
+    z = len(tmp)-1
+    s = tmp[z-1] + tmp[z]
+    if s == "yo":
+        return x
+    else:
+        x = " "
+        return x
+
+with open("answer.txt", "a+") as out:
+    for i in text:
+        i = i.split()
+        f = []
+        for i in i:
+            e = find_ka(i)
+            f += [e]
+        out.write(' '.join(f)+'\n')
+
+a.close()
+
+
+
