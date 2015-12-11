f84df6c3ab773052255be05cddf7ff90c9539278
KateSkomorokhova <ruhesommertag@gmail.com>
2015-12-12 01:38:04 +0300

    task

--- homeworks/homework6/Wiki Game*.py
+++ homeworks/homework6/Wiki Game*.py
@@ -8,26 +8,49 @@ url1 = "https://en.wikipedia.org/wiki/Gone_Maggie_Gone"
 url2 = "https://en.wikipedia.org/wiki/Theia_(planet)"
 new_link = "https://en.wikipedia.org"
 
-def find_link(url1, url2, count):
-    data = requests.get(url1).text
-    parser = etree.HTMLParser()
-    tree = etree.fromstring(data, parser)
+data = requests.get(url1).text
+parser = etree.HTMLParser()
+tree = etree.fromstring(data, parser)
 
-    for element in tree.iter("a"):
-        b = str(element.get('href'))
-        f = re.findall("^/wiki/(.+)", b)
-        if re.findall("^/wiki/(.+)", b) != []:
-            url_new = new_link+"/wiki/"+f[0]
-            if url_new == url2:
-                print(url1)
-                print(url_new)
-                print(url2)
-                print()
+for element in tree.iter("a"):
+    b = str(element.get('href'))
+    f = re.findall("^/wiki/(.+)", b)
+    if re.findall("^/wiki/(.+)", b) != []:
+        url_new = new_link+"/wiki/"+f[0]
+        if url_new == url2:
+            print(url1)
+            print(url2)
+            print()
+            break
 
-            else:
-                count += 1
-                if count != 3:
-                    return find_link(url_new, url2, count)
-
-count = 0
-find_link(url1, url2, count)
+        else:
+            data = requests.get(url_new).text
+            parser = etree.HTMLParser()
+            tree = etree.fromstring(data, parser)
+            for elemen in tree.iter("a"):
+                b = str(elemen.get('href'))
+                f = re.findall("^/wiki/(.+)", b)
+                if re.findall("^/wiki/(.+)", b) != []:
+                    url_new2 = new_link+"/wiki/"+f[0]
+                    if url_new2 == url2:
+                        print(url1)
+                        print(url_new)
+                        print(url2)
+                        print()
+                        break
+                    else:
+                        data = requests.get(url_new2).text
+                        parser = etree.HTMLParser()
+                        tree = etree.fromstring(data, parser)
+                        for element in tree.iter("a"):
+                            b = str(element.get('href'))
+                            f = re.findall("^/wiki/(.+)", b)
+                            if re.findall("^/wiki/(.+)", b) != []:
+                                url_new3 = new_link+"/wiki/"+f[0]
+                                if url_new3 == url2:
+                                    print(url1)
+                                    print(url_new)
+                                    print(url_new2)
+                                    print(url2)
+                                    print()
+                                    break
