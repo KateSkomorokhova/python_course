bfd4099d0694f4261d1e3d5d5da8dafa4d8c7da5
KateSkomorokhova <ruhesommertag@gmail.com>
2015-12-11 17:50:36 +0300

    task

--- homeworks/homework6/Wiki Game*.py
+++ homeworks/homework6/Wiki Game*.py
@@ -8,31 +8,26 @@ url1 = "https://en.wikipedia.org/wiki/Gone_Maggie_Gone"
 url2 = "https://en.wikipedia.org/wiki/Theia_(planet)"
 new_link = "https://en.wikipedia.org"
 
-data = requests.get(url1).text
-parser = etree.HTMLParser()
-tree = etree.fromstring(data, parser)
+def find_link(url1, url2, count):
+    data = requests.get(url1).text
+    parser = etree.HTMLParser()
+    tree = etree.fromstring(data, parser)
 
-for element in tree.iter("a"):
-    b = str(element.get('href'))
-    f = re.findall("^/wiki/(.+)", b)
-    if re.findall("^/wiki/(.+)", b) != []:
-        url_new = new_link+"/wiki/"+f[0]
-        if url_new == url2:
-            print(url_new)
-            print(url2)
-            print()
+    for element in tree.iter("a"):
+        b = str(element.get('href'))
+        f = re.findall("^/wiki/(.+)", b)
+        if re.findall("^/wiki/(.+)", b) != []:
+            url_new = new_link+"/wiki/"+f[0]
+            if url_new == url2:
+                print(url1)
+                print(url_new)
+                print(url2)
+                print()
 
-        else:
-            data = requests.get(url_new).text
-            parser = etree.HTMLParser()
-            tree = etree.fromstring(data, parser)
-            for element in tree.iter("a"):
-                b = str(element.get('href'))
-                f = re.findall("^/wiki/(.+)", b)
-                if re.findall("^/wiki/(.+)", b) != []:
-                    url_new2 = new_link+"/wiki/"+f[0]
-                    if url_new2 == url2:
-                        print(url1)
-                        print(url_new)
-                        print(url_new2)
-                        print()
+            else:
+                count += 1
+                if count != 3:
+                    return find_link(url_new, url2, count)
+
+count = 0
+find_link(url1, url2, count)
